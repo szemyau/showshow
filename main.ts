fd031adde630e62f4d21b69702b7995c06605883
chloe <ssze35192@gmail.com>
2023-08-01 22:38:07 +0800

    add select-category.html

--- main.ts
+++ main.ts
@@ -3,10 +3,12 @@ import {print} from 'listening-on'
 
 let app = express();
 
+app.use("/images", express.static("uploads"));
 app.use(express.static('public'))
 
 app.get('/', (req, res) =>
-res.redirect('/home.html'))
+res.redirect('/category.html'))
+
 
 const port = 8000;
 app.listen(port, () =>
