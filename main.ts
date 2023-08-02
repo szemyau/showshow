5fa760a41a9df577b45ea8fe90ff6337a452ba93
chloe <ssze35192@gmail.com>
2023-08-02 11:56:04 +0800

    testing

--- main.ts
+++ main.ts
@@ -9,10 +9,10 @@ app.use(express.static('public'))
 app.get('/', (req, res) =>
 res.redirect('/select-category.html'))
 
-app.use((req,res) => {
-    res.status(404)
-    res.sendFile(path.resolve('public', '404.html'))
-})
+// app.use((req,res) => {
+//     res.status(404)
+//     res.sendFile(path.resolve('public', '404.html'))
+// })
 const port = 8000;
 app.listen(port, () =>
 print(port))
