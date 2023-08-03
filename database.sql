68fdbb322f96d42c9a097539f950e305247b6f56
Brano <bcchiu305@gmail.com>
2023-08-03 11:13:52 +0800

    database Brano update

--- database.sql
+++ database.sql
@@ -6,5 +6,21 @@ CREATE TABLE "user" (
     password varchar(64) not null,
     role integer not null,
     created_at timestamp not null,
-    updated_at timestamp
-)
+    updated_at timestamp not null
+);
+
+CREATE TABLE "participants_events" (
+    id serial primary key,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (event_id) REFERENCES event(id,)
+    created_at timestamp not null,
+    updated_at timestamp not null
+);
+
+CREATE TABLE "users_preferences" (
+    id serial primary key,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (preference_id) REFERENCES preference(id),
+    created_at timestamp not null,
+    updated_at timestamp not null
+);
