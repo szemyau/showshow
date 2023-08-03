2288ff88f450620c2ebccefe801d610b32f8c979
chloe <ssze35192@gmail.com>
2023-08-03 10:49:15 +0800

    morning

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import express  from 'express'
 import {print} from 'listening-on'
 import { userRoutes } from './user.routes';
+import path from "path";
 
 let app = express();
 app.use(express.urlencoded({ extended: false }))
@@ -9,37 +10,33 @@ app.use("/images", express.static("uploads"));
 app.use(express.static('public'))
 app.use(express.urlencoded())
 
+app.use(userRoutes)
 
 
 app.get('/', (req, res) =>
 res.redirect('/home.html'))
 
 app.get('/select-category', (req, res) =>{
-    console.log(req.query)
+    let category = req.query.category;
+    console.log(category) 
     res.redirect('/home.html')
-}
-)
+})
 
-// app.use('/select-category', (req, res, next) => {
-//   console.log('before using urlencoded', req.url, req.body)
-//   next()o
-// })
-
-// app.use('/select-category', (req, res, next) => {
-//   console.log('after using urlencoded', req.url, req.body)
-//   next()
-// })
+// // // learning params, will delete later
+// app.get('/select-category/:category', (req, res) => {
+//     const selectedCategory = req.params.category;
+//     // Handle the selected category as needed
+//     res.send(`Selected category: ${selectedCategory}`);
+// });
 
 app.get('/signup', (req, res) =>
 res.redirect('/signup'))
 
-app.use(userRoutes)
 
-// app.use((req,res) => {
-//     res.status(404)
-//     res.sendFile(path.resolve('public', '404.html'))
-// })
-
+app.use((req,res) => {
+    res.status(404)
+    res.sendFile(path.resolve('public', '404.html'))
+})
 
 const port = 8000;
 app.listen(port, () =>
--- user.routes.ts
+++ user.routes.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 
+
 export let userRoutes = Router()
 
 export type User = {
