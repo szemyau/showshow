9bffb750998c218e05709944ccd3bad2c691e2ed
Katy Yau <waitingyaukaty@gmail.com>
2023-08-03 10:58:40 +0800

    updated database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,10 @@
-CREATE DATABASE tecky;
+CREATE DATABASE "showshow";
+
+CREATE TABLE "user" (
+    id serial primary key,
+    email varchar(64) not null,
+    password varchar(64) not null,
+    role integer not null,
+    created_at timestamp not null,
+    updated_at timestamp
+)
