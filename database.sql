ad021a6d1f333e45930a5c5f8123dc501c617fcd
Katy Yau <waitingyaukaty@gmail.com>
2023-08-06 17:22:47 +0800

    updated main.ts get /category and category.html to get data from database

--- database.sql
+++ database.sql
@@ -109,3 +109,83 @@ values ('Hiking at Lion Rock', 1, '2023-08-06', '13:00:00', null, 'Lion Rock', 1
 ('Hiking at Victoria Peak', 2, '2023-08-17', '13:00:00', null, 'Victoria Peak', 7, 7, 'active', now(), now());
 
 
+
+
+
+-- UP-TO-DATE FINAL VERSION
+# Visualize on https://erd.surge.sh
+#
+# Relationship Types
+#  -    - one to one
+#  -<   - one to many
+#  >-   - many to one
+#  >-<  - many to many
+#  -0   - one to zero or one
+#  0-   - zero or one to one
+#  0-0  - zero or one to zero or one
+#  -0<  - one to zero or many
+#  >0-  - zero or many to one
+#
+////////////////////////////////////
+
+
+user
+----
+id pk
+email varchar(64)
+password varchar(64)
+role enum('admin','member')
+created_at timestamp
+updated_at timestamp
+
+
+event
+----
+id pk
+creator_id fk >- user.id
+name text
+category_id fk >- category.id
+date date
+time time
+agency varchar(64)
+venue varchar(64)
+vacancy integer
+quota integer
+status enum('active','expired','cancelled')
+created_at timestamp
+updated_at timestamp
+
+
+preference
+-----
+id pk
+category integer
+created_at timestamp
+updated_at timestamp
+
+
+participants_events
+-----
+id pk
+user_id fk >- user.id
+event_id fk >- event.id
+created_at timestamp
+updated_at timestamp
+
+
+users_preferences
+-----
+id pk
+user_id fk >- user.id
+category_id fk >- category.id
+created_at timestamp
+updated_at timestamp
+
+
+# user (497, 355)
+# view: (0, 0)
+# event (864, 639)
+# preference (146, 136)
+# users_preferences (478, 136)
+# zoom: 0.900
+# participants_events (867, 352)
--- main.ts
+++ main.ts
@@ -23,10 +23,23 @@ app.get('/', (req, res) =>
     res.redirect('/home.html'))
 
 app.get('/category', async (req, res) => {
-    let id = req.query.id
-    let data = (await client.query(`select * from preference where id = $1`, [id])).rows
-    res.json(data)
-})
+    try {
+        let id = req.query.id;
+    
+        let categoryData = (await client.query(`SELECT * FROM category WHERE id = $1`, [id])).rows;
+        let eventData = (await client.query(`SELECT * FROM event WHERE category_id = $1`, [id])).rows;
+    
+        let response = {
+        categoryData: categoryData,
+        eventData: eventData
+        };
+    
+        res.json(response);
+    } catch (error) {
+        console.error('Error retrieving category and event data:', error);
+        res.status(500).json({ error: 'Failed to retrieve data' });
+    }
+});
 
 
 
