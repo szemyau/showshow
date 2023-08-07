0c60c21a49efdfa8cc28fbf5f18f102d105acfc7
chloe <ssze35192@gmail.com>
2023-08-07 10:07:14 +0800

    updated common html

--- env.ts
+++ env.ts
@@ -3,10 +3,12 @@ import populateEnv from 'populate-env'
 
 dotenv.config()
 
+// add port 
 export let env = {
   DB_NAME: '',
   DB_USERNAME: '',
   DB_PASSWORD: '',
+  PORT: 8000,
 }
 
 populateEnv(env, { mode: 'halt' })
