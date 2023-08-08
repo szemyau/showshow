20cd2b601008c3a0347800d8a41779e90a862e50
Brano <bcchiu305@gmail.com>
2023-08-08 17:24:29 +0800

    contact table create

--- database-log.sql
+++ database-log.sql
@@ -115,7 +115,7 @@ add column about text;
 
 delete from event;
 insert into event
-(name, about, venue)
+(name, about, venue, contact)
 values
 (
  'yoga101'
@@ -135,6 +135,9 @@ alter table "category" add column image varchar(255);
 -- insert info into table category
 insert into "category" (category, created_at, updated_at, image) values 
 ($1, $2, $3, $4), ($5, $6, $7, $8), ($9, $10, $11, $12), ($13, $14, $15, $16), ($17, $18, $19, $20), ($21, $22, $23, $24)
+
+alter table event
+add column contact text;
 
 
     
