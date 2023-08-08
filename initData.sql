acaa59b677f6a60c8187c4bd311481b0f8185cee
Katy Yau <waitingyaukaty@gmail.com>
2023-08-08 15:02:35 +0800

    updated vacancy and status

--- initData.sql
+++ initData.sql
@@ -1,2 +1,26 @@
--- user---
-insert into user () values ();
+--- user ---
+insert into "user" (email, password, created_at, updated_at, role) values 
+('amy@gmail.com', 'Aaaa1111', now(), now(), 'member'),
+('bob@gmail.com', 'Bbbb1111', now(), now(), 'member'),
+('chris@gmail.com', 'Cccc1111', now(), now(), 'member');
+
+--- event ---
+insert into event (name, creator_id, event_date, event_time, agency, venue, vacancy, quota, status, created_at, updated_at, category_id) values
+('Hiking at Lion Rock', 1, '2023-08-06', '13:00:00', 'AAA Agency', 'Lion Rock', 10, 10, 'active', now(), now(), 1),
+('Hiking at Tai Mo Shan', 2, '2023-08-16', '14:00:00', 'BBB Agency', 'Tai Mo Shan', 5, 5, 'active', now(), now(), 1),
+('Hiking at Victoria Peak', 3, '2023-08-17', '15:00:00', 'CCC Agency', 'Victoria Peak', 7, 7, 'active', now(), now(), 1),
+('Music at Victoria Peak', 1, '2023-08-27', '16:00:00', 'CCC Agency', 'Victoria Peak', 7, 7, 'active', now(), now(), 2);
+
+
+--- category ---
+insert into category
+
+
+--- participants_events ---
+insert into participants_events (user_id, event_id, created_at, updated_at) values
+(2, 1, now(), now()),
+(12, 1, now(), now());
+
+
+--- users_categories ---
+
--- routes/event.routes.ts
+++ routes/event.routes.ts
@@ -11,14 +11,42 @@ eventRoutes.get("/event-list", async (req, res) => {
     let categoryData = (
       await client.query(`SELECT * FROM category WHERE id = $1`, [id])
     ).rows;
+
     let eventData = (
       await client.query(`SELECT * FROM event WHERE category_id = $1`, [id])
     ).rows;
 
-    let response = {
-      categoryData: categoryData,
-      eventData: eventData,
-    };
+    let response: any = [];
+
+    for (let data of eventData) {
+      // calculate vacancy
+      let eventVacancyData = (
+        await client.query(
+          `SELECT COUNT(*) FROM participants_events WHERE event_id = $1`,
+          [data.id]
+        )
+      ).rows;
+      let eventQuotaData = [data.quota];
+
+      // check status logic
+      let eventDate = new Date(data.event_date);
+      let today = new Date();
+      let status: string;
+
+      if (eventDate < today) {
+        status = "expired";
+      } else {
+        status = "active";
+      }
+      console.log({ status });
+
+      data["vacancy"] = eventQuotaData[0] - eventVacancyData[0].count;
+      data["status"] = status;
+
+      response.push(data);
+    }
+
+    console.log(response);
 
     res.json(response);
   } catch (error) {
