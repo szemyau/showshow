3ea9ca51799ee903419d5760dd210c2c7ca6f897
Katy Yau <waitingyaukaty@gmail.com>
2023-08-11 20:22:57 +0800

    added guard.ts, modified db script, etc

--- public/js/category-list.js
+++ public/js/category-list.js
@@ -1,27 +1,49 @@
 async function loadCategoryList() {
-    let res = await fetch("/category-list", {
-      headers: { Accept: "application/json" },
+  const res = await fetch("/category-list", {
+    headers: { Accept: "application/json" },
+  });
+
+  let json = await res.json();
+  if (json.error) {
+    next();
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: json.error,
     });
-
-    let json = await res.json();
-    if (json.error) {
-      alert(json.error);
-      return;
-    }
-    console.log(json);
-
-    let categoryList = document.querySelector(".categoryListContainer");
-
-    for (let category of json) {
-      let node = categoryTemplate.content.cloneNode(true);
-      node.querySelector("img").src = category.image;
-      node.querySelector("img").alt = category.name;
-      node.querySelector("label span").textContent = category.name;
-      node.querySelector("label input").value = category.id;
-      categoryList.appendChild(node);
-    }
-
     return;
   }
+  //   } else {
+  //     const sweetAlertOptions = {
+  //       icon: "error",
+  //       title: "Error",
+  //       text: result.error,
+  //     };
 
-  loadCategoryList();
+  //     // Redirect to the login page when the "OK" button is clicked
+  //     const sweetAlertCallback = () => {
+  //       res.redirect("/login");
+  //     };
+
+  //     // Display the Sweet Alert dialog and execute the callback when the user clicks "OK"
+  //     Swal.fire(sweetAlertOptions).then(sweetAlertCallback);
+  //   }
+  // }
+
+  // return;
+
+  let categoryList = document.querySelector(".categoryListContainer");
+
+  for (let category of json) {
+    let node = categoryTemplate.content.cloneNode(true);
+    node.querySelector("img").src = category.image;
+    node.querySelector("img").alt = category.name;
+    node.querySelector("label span").textContent = category.name;
+    node.querySelector("label input").value = category.id;
+    categoryList.appendChild(node);
+  }
+
+  return;
+}
+
+loadCategoryList();
