08f22b0f412956474152814479fad71997cbd1e9
Katy Yau <waitingyaukaty@gmail.com>
2023-08-13 17:10:04 +0800

    added joined successfully feedback, moved event-fetch to event-detail.js

--- public/js/event-detail.js
+++ public/js/event-detail.js
@@ -12,6 +12,7 @@ async function loadEventDetail() {
   console.log(json.isJoined);
 
   if (json.isJoined) {
+    Swal.fire("Joined successfully!", "See you later!", "success");
     button = document.querySelector("#join-button");
     document.querySelector("#join-button").setAttribute("disabled", "");
   }
@@ -27,3 +28,50 @@ async function loadEventDetail() {
   });
 }
 loadEventDetail();
+
+let parmas = new URLSearchParams(location.search);
+let id = parmas.get("id");
+console.log({ id });
+// join event
+document
+  .querySelector("#join-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const formObject = {
+      name: form.name.value,
+      // people: form.people.value,
+      // date: form.date.value,
+      message: form.message.value,
+      join: true,
+    };
+
+    // ajax to call server
+    const res = await fetch(`/event-detail/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formObject),
+    });
+
+    form.reset();
+    location.reload();
+
+    // get back data once ready
+    const result = await res.json();
+    console.log({ result });
+
+    if (result.error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: result.error,
+      }).then(function () {
+        window.location = "/login.html";
+      });
+      return;
+    }
+    // if no more error, redirect to select category
+  });
